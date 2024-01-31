042008441d2164c88dbf740c92f46ee597b409f7
Sean McGoff <srobertmcgoff@gmail.com>
2024-01-31 16:52:45 +0000

    Morse Code

--- week-2/morse-code/morse-code.ino
+++ week-2/morse-code/morse-code.ino
@@ -1,4 +1,5 @@
 #define LED_PIN 13
+const unsigned long DELAY_TIME = 100;
 
 String morsetable[] = {
     // Letters A-Z (indices 0-25)
@@ -42,17 +43,46 @@ int lookup(char c){
 
 }
 
+// Flashes a character (either '.' or '-')
+void flashMorseChar(char c) {
+  if(c != '.' && c != '-') return; // Error Guarding
+  digitalWrite(LED_PIN, HIGH);
+  if(c=='-') {
+    delay(3*DELAY_TIME); // Dash Time = 3tu
+  } else {
+    delay(DELAY_TIME); // Dot Time = 1tu
+  }
+  digitalWrite(LED_PIN, LOW);
+  delay(DELAY_TIME);
+}
+
+// Flashes a Space (7 time units)
+void flashSpace() {
+  digitalWrite(LED_PIN, LOW);
+  delay(7*DELAY_TIME);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT); // Set LED_PIN to output
+  Serial.begin(9600); // Open Serial Port
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_PIN,HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN,LOW);
-  delay(1000);
+  // Read String
+  String morseWord = Serial.readString();
+  // Set all to Uppercase
+  morseWord.toUpperCase();
+  for(char c : morseWord) {
+    Serial.println(c); // For Debugging mostly
+    if(lookup(c) == -1) {
+      // Incorrect Character
+      flashSpace();
+    } else {
+      // The word exists
+      for(char mc : morsetable[lookup(c)]) {
+        flashMorseChar(mc);
+      }
+    }
+  }
 }
 
